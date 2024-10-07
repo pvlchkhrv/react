6ef2ee91b2f05f45b2994c0f56b12f5ae2c1600d
COHERENT\PavelChukharev <pavelchukharev@coherentsolutions.com>
2024-10-07 19:36:32 +0400

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
+
+export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+    const { paths, mode, isDev } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.output,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
+};
